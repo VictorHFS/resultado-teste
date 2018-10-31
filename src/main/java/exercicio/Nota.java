22921f9ffb44e133367067b2dd7b85dd5d5d411f
VictorHFS <vhfurtuoso@gmail.com>
2018-10-31 12:54:16 -0300

    Create Nota.java

---
+++ src/main/java/exercicio/Nota.java
@@ -0,0 +1,16 @@
+
+
+public class Nota {
+	private final Integer valor;
+	public Nota(Integer valor) {
+		this.valor = valor;
+	}
+
+	public boolean ehMenor(Integer outro) {
+		return valor <= outro;
+	}
+
+	public Integer getValor() {
+		reutnr this.valor;
+	}
+}
